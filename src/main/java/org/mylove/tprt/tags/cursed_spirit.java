c5a61f8966f4c808041c107a40db1c4efd3333f3
TanPowasd <122092488+TanPowasd@users.noreply.github.com>
2025-08-15 18:36:05 +0800

    添加词条
    Signed-off-by: TanPowasd <122092488+TanPowasd@users.noreply.github.com>

---
+++ src/main/java/org/mylove/tprt/tags/cursed_spirit.java
@@ -0,0 +1,38 @@
+package org.mylove.tprt.tags;
+
+import com.ssakura49.sakuratinker.library.damagesource.LegacyDamageSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.registries.ForgeRegistries;
+import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.combat.MeleeDamageModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.combat.MeleeHitModifierHook;
+import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import java.util.UUID;
+
+public class cursed_spirit extends NoLevelsModifier implements MeleeDamageModifierHook {
+    UUID uuid=UUID.fromString("0023BA86-65A2-4B81-AC18-57B995D1C3AB");
+    protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
+        super.registerHooks(hookBuilder);
+        hookBuilder.addHook(this, ModifierHooks.MELEE_DAMAGE);
+    }
+    @Override
+    public float getMeleeDamage(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float baseDamage, float damage) {
+        LivingEntity entity = context.getLivingTarget();
+        if(entity!=null){
+            return 1.5f*damage;
+        }
+        return damage;
+    }
+}
