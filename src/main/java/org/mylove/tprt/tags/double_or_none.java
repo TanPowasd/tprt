46b02dc0eca8c772bb4ef0690c007e0b23f514c0
TanPowasd <122092488+TanPowasd@users.noreply.github.com>
2025-08-17 23:55:57 +0800

    修改，添加
    Signed-off-by: TanPowasd <122092488+TanPowasd@users.noreply.github.com>

--- src/main/java/org/mylove/tprt/tags/double_or_none.java
+++ src/main/java/org/mylove/tprt/tags/double_or_none.java
@@ -14,6 +14,7 @@ import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.modifiers.ModifierId;
 import slimeknights.tconstruct.library.modifiers.hook.armor.ModifyDamageModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.armor.OnAttackedModifierHook;
 import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
 import slimeknights.tconstruct.library.modifiers.modules.technical.SlotInChargeModule;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
@@ -23,21 +24,21 @@ import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
 import java.util.UUID;
 
-public class double_or_none extends NoLevelsModifier implements ModifyDamageModifierHook {
+public class double_or_none extends NoLevelsModifier implements ModifyDamageModifierHook, OnAttackedModifierHook {
     UUID uuid=UUID.fromString("95171EEA-3855-49D8-B44B-9F7B842FEB69");
     protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
         super.registerHooks(hookBuilder);
         hookBuilder.addHook(this, ModifierHooks.MODIFY_DAMAGE);
+        hookBuilder.addHook(this,ModifierHooks.ON_ATTACKED);
        // hookBuilder.addModule(new SlotInChargeModule(SLOT_IN_CHARGE));
     }
     public static MobEffect getEffect(){
         ResourceLocation effectId=new ResourceLocation("tprt","moredamage");
         return ForgeRegistries.MOB_EFFECTS.getValue(effectId);
     }
-
+    public static double ifdouble=Math.random();
     @Override
     public float modifyDamageTaken(IToolStackView iToolStackView, ModifierEntry modifierEntry, EquipmentContext context, EquipmentSlot equipmentSlot, DamageSource damageSource, float amount, boolean isDirectDamage){
-        double ifdouble=Math.random();
         LivingEntity entity =context.getEntity();
         LivingEntity attacker =null;
             if(damageSource.getEntity() instanceof LivingEntity){
@@ -45,35 +46,71 @@ public class double_or_none extends NoLevelsModifier implements ModifyDamageModi
             }
             if (attacker != null && entity != null) {
                 if(ifdouble>=0.8){
-                    MobEffectInstance healthgrow=new MobEffectInstance(MobEffects.REGENERATION,200,1);
-                    entity.addEffect(healthgrow);
                     return amount*2;
                 }
                 else{
-                    MobEffect effect = getEffect();
-                    MobEffectInstance instance = attacker.getEffect(effect);
-                    if(instance==null){
-                        attacker.addEffect(new MobEffectInstance(getEffect(), 200));
-                    }
-                    else{
-                        int amplifier = instance.getAmplifier();
-                        int newAmplifier = amplifier + 1;
-                        int duration = instance.getDuration()+10;
-                        boolean ambient = instance.isAmbient();
-                        boolean visible = instance.isVisible();
-                        boolean showIcon = instance.showIcon();
-                        MobEffectInstance newEffect = new MobEffectInstance(
-                                effect,
-                                duration,
-                                newAmplifier,
-                                ambient,
-                                visible,
-                                showIcon);
-                        attacker.addEffect(newEffect);
-                    }
                     return 0;
                 }
             }
         return amount;
+    }
+    @Override
+    public void onAttacked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
+        LivingEntity entity =context.getEntity();
+        LivingEntity attacker =null;
+        if(source.getEntity() instanceof LivingEntity){
+            attacker = (LivingEntity) source.getEntity();
+        }
+        if(entity!=null&&attacker!=null){
+
+            if(ifdouble>=0.8){
+                MobEffectInstance healthgrow=new MobEffectInstance(MobEffects.REGENERATION,200,1);
+                entity.addEffect(healthgrow);
+            }
+            else{
+                MobEffect effect = getEffect();
+                MobEffectInstance instance = attacker.getEffect(effect);
+                if(instance==null){
+                    attacker.addEffect(new MobEffectInstance(getEffect(), 200));
+                }
+                else{
+                    int amplifier = instance.getAmplifier();
+                    int newAmplifier = amplifier + 1;
+                    int duration = instance.getDuration()+10;
+                    boolean ambient = instance.isAmbient();
+                    boolean visible = instance.isVisible();
+                    boolean showIcon = instance.showIcon();
+                    MobEffectInstance newEffect = new MobEffectInstance(
+                            effect,
+                            duration,
+                            newAmplifier,
+                            ambient,
+                            visible,
+                            showIcon);
+                    attacker.addEffect(newEffect);
+                }
+            }/*
+            MobEffect effect = getEffect();
+            MobEffectInstance instance = attacker.getEffect(effect);
+            if(instance==null){
+                attacker.addEffect(new MobEffectInstance(getEffect(), 200));
+            }
+            else{
+                int amplifier = instance.getAmplifier();
+                int newAmplifier = amplifier + 1;
+                int duration = instance.getDuration()+10;
+                boolean ambient = instance.isAmbient();
+                boolean visible = instance.isVisible();
+                boolean showIcon = instance.showIcon();
+                MobEffectInstance newEffect = new MobEffectInstance(
+                        effect,
+                        duration,
+                        newAmplifier,
+                        ambient,
+                        visible,
+                        showIcon);
+                attacker.addEffect(newEffect);
+            }*/
+        }
     }
 }
