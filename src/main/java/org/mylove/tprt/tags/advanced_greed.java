6505e6c706b1db200c27d3da102a72fc66ccb27b
TanPowasd <wang1422857@outlook.com>
2025-08-28 01:25:49 +0800

    改BUG

--- src/main/java/org/mylove/tprt/tags/advanced_greed.java
+++ src/main/java/org/mylove/tprt/tags/advanced_greed.java
@@ -26,7 +26,7 @@ public class advanced_greed extends NoLevelsModifier implements MeleeDamageModif
         hookBuilder.addHook(this, ModifierHooks.MELEE_DAMAGE);
     }
     public static MobEffect getEffect(){
-        ResourceLocation effectId=new ResourceLocation("tprt","myl_girig_pro.json");
+        ResourceLocation effectId=new ResourceLocation("tprt","myl_girig_pro");
         return ForgeRegistries.MOB_EFFECTS.getValue(effectId);
     }
     @Override
