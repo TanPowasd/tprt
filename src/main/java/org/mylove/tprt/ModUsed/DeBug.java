45a0e57d4eb597e57b9b5f57cfd6735fb9f1c4b3
TanPowasd <wang1422857@outlook.com>
2025-08-27 13:36:51 +0800

    改bug

---
+++ src/main/java/org/mylove/tprt/ModUsed/DeBug.java
@@ -0,0 +1,9 @@
+package org.mylove.tprt.ModUsed;
+
+public class DeBug {
+    public class Logger {
+        public static void log(String message) {
+            System.out.println(message);
+        }
+    }
+}
