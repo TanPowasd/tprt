2ae06d3c4af5e79e41b936b74c6a48b81d489fb1
LR-19 <3350564589@qq.com>
2025-10-02 22:21:48 +0800

    修复了服务端崩溃的bug，隐藏了未完成材料

--- src/main/java/org/mylove/tprt/tags/oxidation.java
+++ src/main/java/org/mylove/tprt/tags/oxidation.java
@@ -17,19 +17,19 @@ public class oxidation extends BaseModifier {
     ResourceLocation oxg_in_tprt_modifier_oxidation=ResourceLocation.fromNamespaceAndPath(Tprt.MODID,"oxg_in_tprt_modifier_oxidation");
     @Override
     public void modifierOnInventoryTick(IToolStackView tool, ModifierEntry modifier, Level level, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack itemStack) {
-        int oxgInTprt_modifier_oxidation =tool.getPersistentData().getInt(oxg_in_tprt_modifier_oxidation);
+        int oxgintprt_modifier_oxidation =tool.getPersistentData().getInt(oxg_in_tprt_modifier_oxidation);
         int maxDurable=tool.getCurrentDurability();
         int nowDurable=0;
         int CFf=maxDurable-nowDurable;
         //氧化值最多是以损失耐久的1/3
-        if(CFf/3< oxgInTprt_modifier_oxidation){
-            oxgInTprt_modifier_oxidation =CFf/3;
+        if(CFf/3< oxgintprt_modifier_oxidation){
+            oxgintprt_modifier_oxidation =CFf/3;
         }
         ticks++;
         if(ticks%60==0){
-            oxgInTprt_modifier_oxidation++;
+            oxgintprt_modifier_oxidation++;
             ticks=0;
         }
-        tool.getPersistentData().putInt(oxg_in_tprt_modifier_oxidation,oxgInTprt_modifier_oxidation);
+        tool.getPersistentData().putInt(oxg_in_tprt_modifier_oxidation,oxgintprt_modifier_oxidation);
     }
 }
