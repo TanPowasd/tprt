cdd54ec0f776247c86c56074526039551aa74084
🐜 <125963716+Heiwenziduo@users.noreply.github.com>
2025-10-05 13:59:59 +0800

    添加属性Attribute的例子

--- src/main/java/org/mylove/tprt/tags/_TestModifier.java
+++ src/main/java/org/mylove/tprt/tags/_TestModifier.java
@@ -1,19 +1,31 @@
 package org.mylove.tprt.tags;
 
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraftforge.common.util.Lazy;
 import org.mylove.tprt.hooks.TprtHooks;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.behavior.AttributesModifierHook;
 import slimeknights.tconstruct.library.modifiers.hook.build.ToolStatsModifierHook;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolContext;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 import slimeknights.tconstruct.library.tools.stat.ModifierStatsBuilder;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
-public class _TestModifier extends Modifier implements ToolStatsModifierHook {
+import java.util.UUID;
+import java.util.function.BiConsumer;
+
+public class _TestModifier extends Modifier implements ToolStatsModifierHook, AttributesModifierHook {
+    private final Lazy<UUID> uuid = Lazy.of(() -> UUID.nameUUIDFromBytes(getId().toString().getBytes()));
+
     @Override
     protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
-        hookBuilder.addHook(this, ModifierHooks.TOOL_STATS);
+        hookBuilder.addHook(this, ModifierHooks.TOOL_STATS, ModifierHooks.ATTRIBUTES);
     }
 
     @Override
@@ -23,5 +35,10 @@ public class _TestModifier extends Modifier implements ToolStatsModifierHook {
         ToolStats.DURABILITY.add(builder, 500f * level);
         ToolStats.ARMOR.add(builder, (double) 3.0f * level);
         ToolStats.ARMOR_TOUGHNESS.add(builder, (double) 2.0f * level);
+    }
+
+    @Override
+    public void addAttributes(IToolStackView tool, ModifierEntry modifier, EquipmentSlot slot, BiConsumer<Attribute, AttributeModifier> consumer) {
+        consumer.accept(Attributes.ATTACK_DAMAGE, new AttributeModifier(uuid.get(), "__description__", 1f, AttributeModifier.Operation.ADDITION));
     }
 }
