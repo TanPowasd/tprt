b87556c43f59b64f9ea148dbf862d5fb3e15a13c
LR-19 <3350564589@qq.com>
2025-10-14 00:52:09 +0800

    完成了远古金属材料，完成了远古遗魂的额外槽位强化

--- src/main/java/org/mylove/tprt/Modifiers/ancient_sandstorm.java
+++ src/main/java/org/mylove/tprt/Modifiers/ancient_sandstorm.java
@@ -1,11 +1,16 @@
 package org.mylove.tprt.Modifiers;
 
-import com.ssakura49.sakuratinker.generic.BaseModifier;
-import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
 import org.mylove.tprt.registries.ModifierIds;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
 import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.build.ToolStatsModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.interaction.GeneralInteractionModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.interaction.InteractionSource;
+import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
 import slimeknights.tconstruct.library.modifiers.modules.build.ModifierRequirementsModule;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolContext;
@@ -13,22 +18,14 @@ import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 import slimeknights.tconstruct.library.tools.stat.ModifierStatsBuilder;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
-public class ancient_sandstorm extends BaseModifier {
+public class ancient_sandstorm extends NoLevelsModifier implements ToolStatsModifierHook, GeneralInteractionModifierHook {
 
     protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
+        super.registerHooks(hookBuilder);
+        hookBuilder.addHook(this, ModifierHooks.TOOL_STATS);
+
         hookBuilder.addHook(this, ModifierHooks.GENERAL_INTERACT);
         hookBuilder.addModule(ModifierRequirementsModule.builder().requireModifier(ModifierIds.STRONG_BUT_PLIABLE, 1).modifierKey(ModifierIds.ANCIENT_SANDSTORM).build());
-    }
-
-    @Override
-    public Component validate(IToolStackView Tool, @NotNull ModifierEntry entry){
-            if (
-                    entry.getLevel() == 1 &&
-                    (Tool.getModifierLevel(ModifierIds.STRONG_BUT_PLIABLE) > 1)
-            ){
-                return Component.translatable("recipe.tprt.modifier.ancient_sandstorm_requirement");
-            }
-            return null;
     }
 
     public void addToolStats(IToolContext context, @NotNull ModifierEntry modifier, @NotNull ModifierStatsBuilder builder) {
@@ -40,9 +37,16 @@ public class ancient_sandstorm extends BaseModifier {
         float Att=builder.getStat(ToolStats.ATTACK_DAMAGE);
         float Pro=builder.getStat(ToolStats.PROJECTILE_DAMAGE);
         float Arm=builder.getStat(ToolStats.ARMOR);
+
         ToolStats.ATTACK_DAMAGE.add(builder, 0.75*Att * z * X);
-        ToolStats.PROJECTILE_DAMAGE.add(builder, 0.75 * Pro * z * X);
-        ToolStats.ARMOR.add(builder, 0.75 * Arm * z * X);
+        ToolStats.PROJECTILE_DAMAGE.add(builder, 1.0 * Pro * z * X);
+        ToolStats.ARMOR.add(builder, 1.25 * Arm * z * X);
+        ToolStats.DURABILITY.add(builder,1000);
+    }
+
+    @Override
+    public InteractionResult onToolUse(IToolStackView tool, ModifierEntry modifier, Player player, InteractionHand hand, InteractionSource source) {
+        return null;
     }
 }
 
--- src/main/java/org/mylove/tprt/Modifiers/strong_but_pliable.java
+++ src/main/java/org/mylove/tprt/Modifiers/strong_but_pliable.java
@@ -1,13 +1,22 @@
 package org.mylove.tprt.Modifiers;
 
-import com.ssakura49.sakuratinker.generic.BaseModifier;
+import org.jetbrains.annotations.NotNull;
+import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.build.ToolStatsModifierHook;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolContext;
 import slimeknights.tconstruct.library.tools.stat.ModifierStatsBuilder;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
-public class strong_but_pliable extends  BaseModifier {
-    public void addToolStats(IToolContext context, ModifierEntry modifier, ModifierStatsBuilder builder) {
+public class strong_but_pliable extends Modifier implements ToolStatsModifierHook {
+
+    protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
+        super.registerHooks(hookBuilder);
+        hookBuilder.addHook(this, ModifierHooks.TOOL_STATS);
+    }
+    public void addToolStats(@NotNull IToolContext context, ModifierEntry modifier, ModifierStatsBuilder builder) {
         int x = modifier.getLevel();
         double X= Math.sqrt(x);
         float y= builder.getStat(ToolStats.DURABILITY);
@@ -16,5 +25,6 @@ public class strong_but_pliable extends  BaseModifier {
         ToolStats.ATTACK_DAMAGE.add(builder, z * X);
         ToolStats.PROJECTILE_DAMAGE.add(builder, 0.75*z * X);
         ToolStats.ARMOR.add(builder, 1.25*z * X);
+        ToolStats.DURABILITY.add(builder,500);
     }
 }
